8844210f5eb6114459b5e4ca665d607c13ae6110
kgalea <kgalea@steamulo.com>
2022-04-03 11:39:00 +0200

    [INIT] Init express server
    - Swagger
    - Routes
    - Libs

---
+++ README.md
@@ -0,0 +1,28 @@
+Weather API
+===
+
+API de l'application météo.
+
+## Overview
+
+Cette API Node utilise express.
+
+## Pré-requis
+
+- NodeJS v16.13
+
+## Git
+
+Messages de commit: "[{type}] {message}" avec type :
+
+- **FEAT** pour _feature_ : nouvelle fonctionnalité
+- **FIX** pour _fix_ : corrections de bugs ou anomalies
+- **IMPR** pour _improvement_ : amélioration d'une feature, refactoring, optimisation
+- **TEST** pour _test_ : ajout ou modification de tests
+- **DOC** pour _documentation_ : ajout ou modification de tests
+
+Exemple: [EVOL] Added button component
+
+## Liens utiles
+
+- [l'api en local](http://localhost:3000)
---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const routes = require('./app/routes/routes');
+
+app.use('/', routes);
+
+// listen for requests
+let listener = app.listen(port, () => {
+    console.log("Server is listening on port "  + port);
+    console.log("API is available on " + listener.address().address + "/api/");
+    console.log("Swagger is available on " + listener.address().address + "/api-docs")
+});
